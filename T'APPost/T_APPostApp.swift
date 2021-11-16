bb465c0d4e96a4dc6be9546f2c9996739135f815
Decoyman24 <minnitiluigi7@gmail.com>
2021-11-16 12:22:38 +0100

    Initial Commit

---
+++ T'APPost/T_APPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_APPostApp.swift
+//  T'APPost
+//
+//  Created by Luigi Minniti on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct T_APPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
