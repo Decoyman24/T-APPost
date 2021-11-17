730f55e7dfa2e7c617c8136d74830b48189d50de
danieledenisi7 <92307268+danieledenisi7@users.noreply.github.com>
2021-11-17 20:11:55 +0100

    Ho aggiunto Dashboard della sezione quiz, prototipo della view del quiz e shape della propic, dovrebbe funzionare tutto, se non funziona significa che ho fallito e domani festeggeremo il mio fallimento con una pizza fritta.

--- T'APPost/Quiz.swift
+++ T'APPost/Quiz.swift
@@ -6,11 +6,71 @@
 //
 
 import SwiftUI
+import Foundation
+
+var categories = ["Greetings", "Daily life", "Romance", "Danger Zone"]
+var levels = ["Chiattillo", "Scugnizzo", "Sarracino", "Cafone"]
+
+
 
 struct QuizDash: View{
+    
+    @State private var unlocked = false
+    
     var body: some View{
-        Text("Ueue chest è o quiz")
-    }
+        
+        VStack(spacing: 20){
+                   
+                    
+                    Text("Quiz")
+                        .font(.largeTitle)
+                        .foregroundColor(Color.blue)
+                        .fontWeight(.heavy)
+                        .padding(5.0)
+//                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                    Spacer()
+                      
+                        circleShape()
+                            
+                    Text("Lv. \(levels[0])").italic()
+                            
+                            Divider()
+                            Spacer()
+            VStack{
+                    Group {
+                            Button("\(categories[0])") {
+                            }
+                
+                            Button("\(categories[1])") {
+                            }
+                            
+                            Button("\(categories[2])") {
+                            }
+
+                            Button("\(categories[3])") {
+                            }
+
+                        }.padding(4).scenePadding(.vertical)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(.blue)
+                        .cornerRadius(25)
+                        .font(.title)
+                        .foregroundColor(Color.white)
+                  }
+//            .disabled(unlocked == false)
+            
+            
+                    Spacer()
+
+                    
+                        
+                    
+//                        .preferredColorScheme(.dark)
+                        
+                }.padding()
+    
+}
 }
 
 struct QuizDash_Previews: PreviewProvider {
---
+++ T'APPost/circleShape.swift
@@ -0,0 +1,28 @@
+//
+//  CircleShape.swift
+//  ueke2
+//
+//  Created by Daniele De Nisi on 12/11/21.
+//
+//  Questa è la struct che crea la shape della propic, in foto Mario Rossi, in attesa delle vere immagini.
+
+import SwiftUI
+
+struct circleShape: View {
+    var body: some View {
+       Image ("MarioRossi-1")
+            .resizable()
+            .frame(width: 150, height: 150)
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.blue, lineWidth: 2)
+                       }
+            
+    }
+}
+
+struct circleShape_Previews: PreviewProvider {
+    static var previews: some View {
+        circleShape()
+    }
+}
