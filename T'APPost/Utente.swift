aa20582cd2fe6ffa29dc7151cc0ee9154cc81b02
Decoyman24 <minnitiluigi7@gmail.com>
2021-11-18 12:10:45 +0100

    - aggiunto la struct Utente con tutte le sue variabili associate, mamm vost

---
+++ T'APPost/Utente.swift
@@ -0,0 +1,24 @@
+//
+//  Utente.swift
+//  T'APPost
+//
+//  Created by Luigi Minniti on 18/11/21.
+//
+
+import Foundation
+var categories = ["Greetings", "Daily life", "Romance", "Danger Zone"]
+// I livelli sono: Chiattillo, Scugnizzo, Sarracino, Cafone
+
+struct Utente {
+    var greetingsUnlocked : Bool
+    var dailyLifeUnlocked: Bool
+    var romanceUnlocked: Bool
+    var dangerZoneUnlocked: Bool
+    var progress: Int64
+    var profilePic : String
+    var userTitle: String
+}
+
+var ourUser = Utente(greetingsUnlocked: true, dailyLifeUnlocked: false, romanceUnlocked: false, dangerZoneUnlocked: false, progress: 0, profilePic: "chiattillo", userTitle: "Chiattillo")
+
+
